02d2b95b46fbd3906a97a0e70220c5a19b1bf5e0
Lars17S <efren.garibaldi@gmail.com>
2022-09-27 18:21:11 -0500

    Added graphql and storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,18 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "poolappserviceaws": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "s372608a89": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
