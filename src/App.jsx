2ea7dc92f5261aec3b3d734b80e29db59c5b3e64
Lars17S <efren.garibaldi@gmail.com>
2022-09-28 00:37:52 -0500

    oauth v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,48 @@
-import logo from "./logo.svg";
 import "@aws-amplify/ui-react/styles.css";
-import {
-  withAuthenticator,
-  Button,
-  Heading,
-  Image,
-  View,
-  Card,
-} from "@aws-amplify/ui-react";
+import React, { useEffect, useState } from "react";
+import { Auth, Hub } from "aws-amplify";
 
-function App({ signOut, user }) {
+function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    Hub.listen("auth", ({ payload: { event, data } }) => {
+      switch (event) {
+        case "signIn":
+        case "cognitoHostedUI":
+          getUser().then((userData) => setUser(userData));
+          break;
+        case "signOut":
+          setUser(null);
+          break;
+        case "signIn_failure":
+        case "cognitoHostedUI_failure":
+          console.log("Sign in failure", data);
+          break;
+      }
+    });
+
+    getUser().then((userData) => setUser(userData));
+  }, []);
+
+  function getUser() {
+    return Auth.currentAuthenticatedUser()
+      .then((userData) => userData)
+      .catch(() => console.log("Not signed in"));
+  }
+
   return (
-    <View className="App">
-      <Card>
-        <Image src={logo} className="App-logo" alt="logo" />
-        <Heading level={1}>We now have Auth!</Heading>
-      </Card>
-      <h1>Hello {user.username}, welcome.</h1>
-      <Button onClick={signOut}>Sign Out</Button>
-    </View>
+    <div>
+      <p>User: {user ? JSON.stringify(user.attributes) : "None"}</p>
+      {user ? (
+        <button onClick={() => Auth.signOut()}>Sign Out</button>
+      ) : (
+        <button onClick={() => Auth.federatedSignIn()}>
+          Federated Sign In
+        </button>
+      )}
+    </div>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -6,6 +6,9 @@ import reportWebVitals from "./reportWebVitals";
 import { Amplify } from "aws-amplify";
 import config from "./aws-exports";
 
+config.oauth.redirectSignIn = `${window.location.origin}/`;
+config.oauth.redirectSignOut = `${window.location.origin}/`;
+
 Amplify.configure(config);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
